06750e075eada45826ffe28fd93811ec6712fdae
julio.perdomo <jperdomocorrea@gmail.com>
2023-03-27 12:18:27 -0400

    ADD License File

--- config/database.ts
+++ config/database.ts
@@ -4,9 +4,9 @@ export default ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'tiendi'),
-      user: env('DATABASE_USERNAME', 'caribito'),
-      password: env('DATABASE_PASSWORD', 'caribito'),
+      database: env('DATABASE_NAME', 'database'),
+      user: env('DATABASE_USERNAME', 'db_user'),
+      password: env('DATABASE_PASSWORD', 'db_password'),
       ssl: env.bool('DATABASE_SSL', false),
     },
     debug: env.bool('DATABASE_DEBUG', false),
